d5f11b1c2ce21ecb55b8bc6ff16c13ebb4360439
Maximilien_Fathi <maximilien.fathi.17@gmail.com>
2020-12-10 21:19:37 -0500

    Assignment done, need to check for mistakes though

--- driver.py
+++ driver.py
@@ -10,8 +10,8 @@ from OV_model import *
 from FV_model import *
 import pandas as pd
 
-train_df = pd.read_csv("covid_training.tsv", sep='\t', header=None)
-test_df = pd.read_csv("covid_test_public.tsv", sep='\t', header=None)
+train_df = pd.read_csv("Input_Files/covid_training.tsv", sep='\t', header=None)
+test_df = pd.read_csv("Input_Files/covid_test_public.tsv", sep='\t', header=None)
 
 OV_clf = OV_model(train_df)
 OV_clf.create_original_vocab()
